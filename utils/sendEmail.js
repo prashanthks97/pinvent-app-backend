ac4bda68da93d38eb358fc5d9cfe834f0c2b3ea6
Prashanth K S <prashanthsomshekar1997@gmail.com>
2026-02-03 11:18:48 +0530

    latest

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -26,9 +26,7 @@ const sendEmail = async (subject, message, send_to, sent_from, reply_to) => {
   // send email
   transporter.sendMail(options, function (err, info) {
     if (err) {
-      console.log(err);
     } else {
-      console.log(info);
     }
   });
 };
